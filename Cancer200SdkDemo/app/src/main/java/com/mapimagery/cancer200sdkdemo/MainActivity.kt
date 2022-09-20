c900c34775bb9690598d482698b958236a80d7d1
Andrew Hunt <andrew.hunt@team.telstra.com>
2022-09-20 17:19:16 +0800

    Update the libraries to latest and update demo to use Fragments

--- Cancer200SdkDemo/app/src/main/java/com/mapimagery/cancer200sdkdemo/MainActivity.kt
+++ Cancer200SdkDemo/app/src/main/java/com/mapimagery/cancer200sdkdemo/MainActivity.kt
@@ -1,22 +1,45 @@
 package com.mapimagery.cancer200sdkdemo
 
 import android.app.Activity
-import android.content.Intent
+import android.app.FragmentManager
+import android.app.FragmentTransaction
 import android.os.Bundle
 import com.mapswim.cancer200sdk.Cancer200
-import com.mapswim.cancer200sdk.MapActivity
+import com.mapswim.cancer200sdk.MapSwimMapFragment
+
+
+
 
 class MainActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Init the Map Api
         Cancer200.initApi(this)
 
-        // Launch the map
-        val mapActivity = MapActivity()
-        val mapIntent = Intent(this, MapActivity::class.java)
-        startActivity(mapIntent)
+        setContentView(R.layout.map_activity_container)
+
+        if (savedInstanceState == null) {
+            val fragMan: FragmentManager? = this.fragmentManager
+            val fragTransaction: FragmentTransaction? = fragMan?.beginTransaction()
+            fragTransaction?.add(R.id.fragment_container_view, MapSwimMapFragment())
+            fragTransaction?.commit()
+        }
     }
 }
+
+// Alternative integration via public MainActivty from the library
+//class MainActivity : Activity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        // Init the Map Api
+//        Cancer200.initApi(this)
+//
+//        // Launch the map
+//        val mapActivity = MapActivity()
+//        val mapIntent = Intent(this, MapActivity::class.java)
+//        startActivity(mapIntent)
+//    }
+//}
